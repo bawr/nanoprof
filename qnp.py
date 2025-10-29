407e3e34f1bbd0f6fa51c9d5695e34ed0191f93d
Bartosz Wróblewski <b@wr.wtf>
2025-10-29 01:11:26 -0700

    Better exporter.
    TODO: Fill start / end values from thread records.

--- qnp.py
+++ qnp.py
@@ -1,31 +1,70 @@
 #!/usr/bin/env python3
 
+import json
 import qnd
 
 FILE = qnd.Qnd.from_file("./test/profile.qnd")
-CODE = {}
-TREE = {}
 
-def print_time(trec: qnd.Qnd.Time):
-    trep = []
-    nrec = TREE.get(trec.node_id)
-    while nrec:
-        trep.append("%s:%s:%d" % CODE[nrec[2]])
-        nrec = TREE.get(nrec[1])
-    stem = ";".join(trep[::-1])
-    if trec.time_active:
-        print("%s;THREAD:%d;ACTIVE %d" % (stem, trec.thread_id, trec.time_active * 1e-6))
-    if trec.time_paused:
-        print("%s;THREAD:%d;PAUSED %d" % (stem, trec.thread_id, trec.time_paused * 1e-6))
+NODE_TO_PREV_IDX = {}
+NODE_TO_CODE_IDX = {
+    0: 0,
+}
+CODE_ADDR_TO_IDX = {
+    0: 0,
+}
+CODE_IDX_TO_INFO = [
+    {
+        "name": "$",
+    },
+]
+NODE_TO_CODE_LST = [
+    [0],
+]
+
+JSON_PROFILES = {}
+JSON = {
+    "$schema": "https://www.speedscope.app/file-format-schema.json",
+    "name": "$",
+    "shared": {
+        "frames": None,
+    },
+    "profiles": None,
+}
+
 
 if __name__ == "__main__":
     for raw in FILE.rec_list:
         rec = raw.rec
         if isinstance(rec, qnd.Qnd.Code):
-            CODE[rec.ptr] = (rec.filename.text, rec.qualname.text, rec.line)
+            CODE_ADDR_TO_IDX[rec.ptr] = len(CODE_IDX_TO_INFO)
+            CODE_IDX_TO_INFO.append({
+                "name": rec.qualname.text,
+                "file": rec.filename.text,
+                "line": rec.line,
+            })
         if isinstance(rec, qnd.Qnd.Node):
-            TREE[rec.node_id] = (rec.node_id, rec.caller_id, rec.ptr)
+            node = rec.node_id
+            NODE_TO_PREV_IDX[node] = rec.caller_id
+            NODE_TO_CODE_IDX[node] = CODE_ADDR_TO_IDX[rec.ptr]
+            code_lst = []
+            while node:
+                code_lst.append(NODE_TO_CODE_IDX[node])
+                node = NODE_TO_PREV_IDX[node]
+            NODE_TO_CODE_LST.append(code_lst[::-1])
         if isinstance(rec, qnd.Qnd.Emit):
             pass # print(rec.time * 1e-9)
         if isinstance(rec, qnd.Qnd.Time):
-            print_time(rec)
+            profile = JSON_PROFILES.setdefault(rec.thread_id, {
+                "type": "sampled",
+                "name": ("THREAD[%d]" % rec.thread_id),
+                "unit": "seconds",
+                "samples": [],
+                "weights": [],
+                "startValue": 0,
+                "endValue": 30,
+            })
+            profile["samples"].append(NODE_TO_CODE_LST[rec.node_id])
+            profile["weights"].append((rec.time_active + rec.time_paused) * 1e-9)
+    JSON["profiles"] = list(JSON_PROFILES.values())
+    JSON["shared"]["frames"] = CODE_IDX_TO_INFO
+    print(json.dumps(JSON))
